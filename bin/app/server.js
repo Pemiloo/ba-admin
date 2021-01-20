92f1d81c1dab3939a42207ec4d7a4b4320d738cc
Reza Andriansyah <reza@Reza>
2021-01-21 02:40:31 +0700

    new

--- bin/app/server.js
+++
@@ -1,8 +0,0 @@
-const express = require('express').Router;
-const app = new express();
-
-app.all('*', (req, res) => {
-    res.status(404).json({ status: 'false', message: 'Endpoint not found' });
-});
-
-module.exports = app;
